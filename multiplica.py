b65e5d6505842743d8d4d3694be1a108ae961013
pcastillol <pablormcf@gmail.com>
2018-02-05 21:33:42 +0100

    Tablas de multiplicar v0

---
+++ multiplica.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for numero1 in range(1,11):
+    print("La tabla del ", numero1)
+    for numero2 in range(1,11):
+        print(numero1, "por", numero2, "es", numero1*numero2)
